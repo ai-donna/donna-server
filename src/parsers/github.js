a58a14dfbdd1f13213d7c3a6e425143502db27fd
Leandro Silva <leandro@beamery.com>
2017-07-15 18:11:14 +0100

    Improved github parser

--- src/parsers/github.js
+++ src/parsers/github.js
@@ -6,34 +6,32 @@ import Promise from 'promise'
 
 class GithubParser extends GenericParser {
 
-  constructor() {
-    super();
-    console.log("I am a Github parser");
+  constructor () {
+    super()
+    console.log('I am a Github parser')
+    this._githubScraper = Promise.denodeify(githubScraper)
   }
 
-  parse(url) {
-
-    Promise.denodify(githubScraper)(url).then(response => {
-
+  parse = (url) =>
+    Promise.resolve(url)
+      .then(this._githubScraper.bind(this))
+      .then((response) => {
       var options = {
         uri: url,
         transform: function (body) {
-          return cheerio.load(body);
+          return cheerio.load(body)
         }
-      };
+      }
 
-      request(options).then(data => {
-
-        let $          = cheerio.load(data);
-
-        response.html  = $("#readme").html();
-        response.desc  = $(".repository-meta-content").text();
-        response.title = $("#readme h1").first().text();
-        console.log(response);
-        return Promise.resolve(response);
-      });
+      return request(options)
+        .then($ => {
+          response.html  = $("#readme").html();
+          response.desc  = $(".repository-meta-content").text();
+          response.title = $("#readme h1").first().text();
+          console.log(response);
+          return Promise.resolve(response)
+        })
     })
-  }
 }
 
 export default GithubParser

fdc3b6627b39632936a5af4b87ecaf2e5f221840
Leandro Silva <leandro@beamery.com>
2017-07-15 22:17:24 +0100

    Minor improvements on nlp ask

--- src/api/nlp/controller.js
+++ src/api/nlp/controller.js
@@ -1,12 +1,8 @@
 import apiai from 'apiai'
 import { success } from '../../services/response/'
 import uuid from 'uuid/v4'
-import Promise from 'promise'
 
 const app = apiai('749af6a1d2634e209dc08f1ee2521c3b')
-//const app = apiai('e9487e979f614009838222984498c608')
-
-const textRequest = Promise.denodeify(app.textRequest)
 
 export const ask = ({ body }, res, next) => {
   const request = app.textRequest(body.question, {

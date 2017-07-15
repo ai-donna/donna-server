3ce31d25e832c17fa9578ad839fbdfe19d26e188
Leandro Silva <leandro@beamery.com>
2017-07-15 17:41:26 +0100

    Improved youtube parser

--- src/parsers/youtube.js
+++ src/parsers/youtube.js
@@ -1,9 +1,39 @@
-import request from 'request-promise'
 import GenericParser from './generic'
+import Youtube from 'youtube-node'
+import Promise from 'promise'
+
+const YOUTUBE_KEY = 'AIzaSyDjnFOup378dHY1HaQCWToCkNc6UieZVW4'
 
 class YoutubeParser extends GenericParser {
+  constructor () {
+    super()
+    this.Youtube = new Youtube()
+    this.Youtube.setKey(YOUTUBE_KEY)
+    this._getById = Promise.denodeify(this.Youtube.getById)
+  }
+
   parse = (url) =>
-    request(url)
+    Promise.resolve(url)
+      .then(this._getVideoId.bind(this))
+      .then(this._print.bind(this))
+      .then(this._getById.bind(this))
+
+  _getVideoId = (url) =>
+    Promise.resolve(url)
+      .then((url) => {
+        var id = url.split('v=')[1]
+        var ampersandPosition = id.indexOf('&')
+        if (ampersandPosition !== -1) {
+          id = id.substring(0, ampersandPosition)
+        }
+        return id
+      })
+
+  _print = (str) => {
+    console.log(str)
+    return str
+  }
+
 }
 
 export default YoutubeParser
